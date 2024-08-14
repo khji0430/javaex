82ae0c64e4646d20e5805d6002365a17cbc3f2a7
hwan <jikh0430@naver.com>
2024-08-14 15:53:02 +0900

    code add4

--- src/basic/MethodDemo3.java
+++ src/basic/MethodDemo3.java
@@ -12,31 +12,23 @@ public class MethodDemo3 {
 //    result = 100 + 200;
 //    System.out.println(result);
 
-    System.out.println(add(2, 3));
-    System.out.println(add(10, 20));
-    System.out.println(add(100, 200));
 
-    System.out.println(minus(2, 3));
-    System.out.println(minus(10, 20));
-    System.out.println(minus(100, 200));
+    // method의 이름은 minus, 매개변수는 2개이고 데이터타입은 int
+    //  첫번째 매개변수에서 두번째 매개변수를 빼서 그 결과를 반환하는 메소드를 만들
+    // 메서드의 반환타입은 int
+    // minus(2,3) 출력 ; minus(20,10)  minus(100,200)
 
-    System.out.println(mul(2, 3));
-    System.out.println(mul(10, 20));
-    System.out.println(mul(100, 200));
+    int a = 3, b = 2;  // 코드에 재사용성이 높아진다.
+    System.out.println(add(a, b));
+    System.out.println(minus(a, b));
+    System.out.println(mul(a, b));
+    System.out.println(div(a, b));
 
-    System.out.println(div(2, 3));
-    System.out.println(div(10, 20));
-    System.out.println(div(100, 200));
   }
     public static int add (int num1, int num2) {
 
         return num1 + num2;
       }
-
-  // method의 이름은 minus, 매개변수는 2개이고 데이터타입은 int
-  //  첫번째 매개변수에서 두번째 매개변수를 빼서 그 결과를 반환하는 메소드를 만들
-  // 메서드의 반환타입은 int
-  // minus(2,3) 출력 ; minus(20,10)  minus(100,200)
 
     public  static  int minus(int num1, int num2){
 
@@ -49,7 +41,7 @@ public class MethodDemo3 {
     }
 
     public  static  int div(int num1, int num2){
-
+      if(num2 == 0) return 0;
       return num1 / num2;
   }
 
